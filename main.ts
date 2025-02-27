8c04a0ce0f53ed9682ee9a36a2e60ca2bf785624
Erik R <erik.riklund@outlook.com>
2025-02-27 22:16:34 +0100

    feat(gitignore): add .artifacts folder

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { createServer } from '#gear:server';
 /**
  * The main entry point for the application.
  * 
- * This file is responsible for creating and starting the server. It does not
+ * This file is responsible for creating and starting the server. It should not
  * contain any application logic, and is only used to bootstrap the server.
  */
 export default await createServer().start();
